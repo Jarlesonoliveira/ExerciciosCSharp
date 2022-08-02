a592701673f863694c48a9a1b0823c896aa4a2f1
Jarleson oliveira <jarlesonmello@gmail.com>
2022-08-02 10:57:53 -0300

    exercicio 1006 resolvido

--- Exec_1006/Program.cs
+++ Exec_1006/Program.cs
@@ -3,14 +3,15 @@
 namespace Exec_1006 {
     class Program {
         static void Main(string[] args) {
-            double NOTAPESO1, NOTAPESO2, MEDIA;
+            double NOTAPESO1, NOTAPESO2, NOTAPESO3, MEDIA;
 
             NOTAPESO1 = Convert.ToDouble(Console.ReadLine());
             NOTAPESO2 = Convert.ToDouble(Console.ReadLine());
+            NOTAPESO3 = Convert.ToDouble(Console.ReadLine());
 
-            MEDIA = ((NOTAPESO1 * 3.5 + NOTAPESO2 * 7.5) / (3.5 + 7.5));
+            MEDIA = ((NOTAPESO1 * 2.0 + NOTAPESO2 * 3.0 + NOTAPESO3 * 5.0) /(10));//10 e a soma de todos os pesos
 
-            Console.WriteLine("MEDIA = " + MEDIA.ToString("F5"));
+            Console.WriteLine("MEDIA = " + MEDIA.ToString("F1"));
             Console.ReadLine();
         }
     }
