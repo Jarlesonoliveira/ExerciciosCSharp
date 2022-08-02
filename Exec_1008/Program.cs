c02f0b8ebe05687ce1deb1e703a92851a8dc2237
Jarleson oliveira <jarlesonmello@gmail.com>
2022-08-02 17:45:33 -0300

    exercicio 1009 resolvido e adicionado o projeto exec_1010

--- Exec_1008/Program.cs
+++ Exec_1008/Program.cs
@@ -1,9 +1,32 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Exec_1008 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            //using System.Globalization; | CultureInfo.InvariantCulture);
+            //Para modificar para 550,00
+            int funcionario;
+            int horas;
+            decimal valor;
+
+            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+
+
+
+            funcionario = int.Parse(Console.ReadLine());
+            horas = int.Parse(Console.ReadLine());
+            valor = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            decimal salario = valor * horas;
+
+            Console.WriteLine("NÚMERO = " + funcionario);
+            Console.WriteLine("SALÁRIO = U$ " + String.Format("{0:0.00}", salario), CultureInfo.InvariantCulture);
+            Console.ReadKey();
         }
     }
 }
---
+++ Exec_1010/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Exec_1010 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
