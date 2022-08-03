a32e167cff6b2284ebb35473f66cfab890071d4d
Jarleson oliveira <jarlesonmello@gmail.com>
2022-08-03 11:53:38 -0300

    exercicio 1009 resolvido

--- Exec_1008/Program.cs
+++ Exec_1008/Program.cs
@@ -1,31 +1,21 @@
 ﻿using System;
-using System.Globalization;
 
 namespace Exec_1008 {
     class Program {
         static void Main(string[] args) {
-            //using System.Globalization; | CultureInfo.InvariantCulture);
-            //Para modificar para 550,00
             int funcionario;
             int horas;
             decimal valor;
 
-            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
-
-            customCulture.NumberFormat.NumberDecimalSeparator = ".";
-
-            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
-
-
 
             funcionario = int.Parse(Console.ReadLine());
             horas = int.Parse(Console.ReadLine());
-            valor = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            valor = decimal.Parse(Console.ReadLine().Replace(".", ","));
 
             decimal salario = valor * horas;
 
             Console.WriteLine("NÚMERO = " + funcionario);
-            Console.WriteLine("SALÁRIO = U$ " + String.Format("{0:0.00}", salario), CultureInfo.InvariantCulture);
+            Console.WriteLine("SALÁRIO = U$ " + salario.ToString().Replace(",", "."));
             Console.ReadKey();
         }
     }
--- Exec_1009/Program.cs
+++ Exec_1009/Program.cs
@@ -3,7 +3,16 @@
 namespace Exec_1009 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            string nome = (Console.ReadLine());
+            double salario = double.Parse(Console.ReadLine());
+            double montante = double.Parse(Console.ReadLine());
+
+            double resultado;
+            resultado = salario + montante * 0.15;
+
+            Console.WriteLine("TOTAL = R$ " + resultado.ToString("F2"));
+            Console.ReadKey();
         }
     }
 }
