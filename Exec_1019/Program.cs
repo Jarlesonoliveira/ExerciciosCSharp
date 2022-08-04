38709ce233015db4c83083ac1c3e3bb3cfa8dc9d
Jarleson oliveira <jarlesonmello@gmail.com>
2022-08-04 17:36:20 -0300

    exercicio 1020 resolvido é 1021 criado

--- Exec_1019/Program.cs
+++ Exec_1019/Program.cs
@@ -9,6 +9,20 @@ namespace Exec_1019
     internal class Program
     {
         static void Main(string[] args) {
+
+            int N, horas, minutos, segundos, resto;
+
+            N = int.Parse(Console.ReadLine());
+
+            horas = N / 3600;
+            resto = N % 3600;
+
+            minutos = resto / 60;
+            segundos = resto % 60;
+
+            Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+            Console.ReadKey();
+
         }
     }
 }
---
+++ Exec_1020/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_1020
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+            int quociente, idade, resto;
+            int ano = 365;
+            int mes = 30;
+
+            idade = int.Parse(Console.ReadLine());
+
+            quociente = idade / ano;
+            Console.WriteLine(quociente + " ano(s)");
+            resto = idade % ano;
+
+            quociente = resto / mes;
+            Console.WriteLine(quociente + " mes(es)");
+            resto = resto % mes;
+
+            Console.WriteLine(resto + " dia(s)");
+            Console.ReadKey();
+        }
+    }
+}
