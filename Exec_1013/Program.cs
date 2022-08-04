d277150c538ecacf726ae168fd66a4a6773aca06
Jarleson oliveira <jarlesonmello@gmail.com>
2022-08-04 09:25:19 -0300

    exercicio 1013 resolvido

--- Exec_1013/Program.cs
+++ Exec_1013/Program.cs
@@ -9,7 +9,21 @@ namespace Exec_1013
     internal class Program
     {
         static void Main(string[] args) {
+            int a, b, c;
 
+            string[] numeros = Console.ReadLine().Split();
+            a = int.Parse(numeros[0]);
+            b = int.Parse(numeros[1]);
+            c = int.Parse(numeros[2]);
+
+            int MaiorAB = (a + b + Math.Abs(a - b)) / 2;
+
+            if (MaiorAB > c) {
+                Console.WriteLine($"{MaiorAB} eh o maior");
+            } else {
+                Console.WriteLine($"{c} eh o maior");
+            }
+            Console.ReadKey();
         }
     }
 }
