ef14d5483886973a3b6fac2894cb67aca741250c
Jarleson oliveira <jarlesonmello@gmail.com>
2022-08-05 18:35:49 -0300

    correções nos arquivos 1000 e 1001

---
+++ Exec_1001/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_1001
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            double area, n, raio;
+            n = 3.14159;
+
+            raio = double.Parse(Console.ReadLine());
+            area = n * (raio * raio);
+
+            Console.WriteLine("A=" + area.ToString("F4"));
+        }
+    }
+}
