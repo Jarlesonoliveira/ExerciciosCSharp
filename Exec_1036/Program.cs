6bbba49de495de657a52ea507da15114cfb006ba
Jarleson oliveira <jarlesonmello@gmail.com>
2022-08-05 17:59:58 -0300

    Exercicio 1036 resolvido

--- Exec_1036/Program.cs
+++ Exec_1036/Program.cs
@@ -10,6 +10,27 @@ namespace Exec_1036
     {
         static void Main(string[] args) {
 
+            double a, b, c, delta, r1, r2;
+
+            string[] leitura = Console.ReadLine().Split(' ');
+             a = double.Parse(leitura[0]);
+             b = double.Parse(leitura[1]);
+             c = double.Parse(leitura[2]);
+
+            delta = Math.Pow(b, 2.0) - 4 * a * c;
+
+            if (a == 0.0 || delta < 0) {
+
+                Console.WriteLine("Impossivel calcular");
+
+            }else 
+            {
+               r1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+               r2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+
+                Console.WriteLine("R1 = " + r1.ToString("F5"));
+                Console.WriteLine("R2 = " + r2.ToString("F5"));
+            }
         }
     }
 }
