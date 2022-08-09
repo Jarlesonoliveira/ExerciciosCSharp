806ae0eb977e79789501c57e48bb05c52bb79c11
Jarleson oliveira <jarlesonmello@gmail.com>
2022-08-09 17:02:47 -0300

    resolvido o exec 1040 é criado o exec 1041

--- Exec_1038/Program.cs
+++ Exec_1038/Program.cs
@@ -9,6 +9,31 @@ namespace Exec_1038
     internal class Program
     {
         static void Main(string[] args) {
+
+            string[] leitura = Console.ReadLine().Split(' ');
+            string item = (leitura[0]);
+            int quantidade = int.Parse(leitura[1]);
+
+            if (item == "1")
+            {
+                Console.WriteLine("Total: R$ " + (quantidade * 4.00).ToString("F2"));
+            } 
+            else if (item == "2")
+            {
+                Console.WriteLine("Total: R$ " + (quantidade * 4.50).ToString("F2"));
+            } 
+            else if (item == "3") 
+            {
+                Console.WriteLine("Total: R$ " + (quantidade * 5.00).ToString("F2"));
+            } 
+            else if (item == "4")
+            {
+                Console.WriteLine("Total: R$ " + (quantidade * 2.00).ToString("F2"));
+            } 
+            else if (item == "5") 
+            {
+                Console.WriteLine("Total: R$ " + (quantidade * 1.50).ToString("F2"));
+            }            
         }
     }
 }
