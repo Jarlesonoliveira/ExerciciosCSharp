10c51b578e74d5aa4f4cc17e3a6c7aad29ba79d6
Jarleson oliveira <jarlesonmello@gmail.com>
2022-08-09 18:25:09 -0300

    resolvido o exec 1041 é criado o exec 1042

--- Exec_1041/Program.cs
+++ Exec_1041/Program.cs
@@ -3,12 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Exec_1041
 {
     internal class Program
     {
         static void Main(string[] args) {
+
+            double X, Y;
+
+            string[] numeros = Console.ReadLine().Split(' ');
+
+            X = double.Parse(numeros[0], CultureInfo.InvariantCulture);
+            Y = double.Parse(numeros[1], CultureInfo.InvariantCulture);
+
+            if (X == 0.0 && Y == 0.0) {
+                Console.WriteLine("Origem");
+            } else if (X == 0.0) {
+                Console.WriteLine("Eixo Y");
+            } else if (Y == 0.0) {
+                Console.WriteLine("Eixo X");
+            } else if (X > 0.0 && Y > 0.0) {
+                Console.WriteLine("Q1");
+            } else if (X < 0.0 && Y > 0.0) {
+                Console.WriteLine("Q2");
+            } else if (X < 0.0 && Y < 0.0) {
+                Console.WriteLine("Q3");
+            } else {
+                Console.WriteLine("Q4");
+            }
         }
     }
 }
