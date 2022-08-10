0fc3018dc46798b5690db08018a38c8b0ba12321
Jarleson oliveira <jarlesonmello@gmail.com>
2022-08-10 16:08:35 -0300

    resolvido o exec 1044 é criado o exec 1045

--- Exec_1044/Program.cs
+++ Exec_1044/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,17 @@ namespace Exec_1044
     internal class Program
     {
         static void Main(string[] args) {
+
+            string[] numeros = Console.ReadLine().Split(' ');
+
+            int A = int.Parse(numeros[0]);
+            int B = int.Parse(numeros[1]);
+
+            if (A % B == 0 || B % A == 0)  {
+                Console.WriteLine("Sao Multiplos");
+            } else {
+                Console.WriteLine("Nao sao Multiplos");
+            }
         }
     }
 }
