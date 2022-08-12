6d52f96be3631ad6161cd627e15f8ebf950b8126
Jarleson oliveira <jarlesonmello@gmail.com>
2022-08-12 13:47:24 -0300

    projeto Exec 1047 resolvido e criado o projeto Exec 1048

--- Exec_1047/Program.cs
+++ Exec_1047/Program.cs
@@ -16,9 +16,25 @@ namespace Exec_1047
             int minutoInicial = int.Parse(vet[1]);
             int horaFinal = int.Parse(vet[2]);
             int minutoFinal = int.Parse(vet[3]);
-            int calculoHoraInicial, calculoHoraFinal, calculoTempoEvento = 0;
 
+            int instanciaInicial = horaInicial * 60 + minutoInicial;
+            int instanciaFinal = horaFinal * 60 + minutoFinal;
 
+            int duracao;
+            if (instanciaInicial < instanciaFinal) {
+
+                duracao = instanciaFinal - instanciaInicial;
+
+            }else {
+
+                duracao = (24 * 60 - instanciaInicial) + instanciaFinal;
+
+            }
+
+            int duracaoHoras = duracao / 60;
+            int duracaoMinutos = duracao % 60;
+
+            Console.WriteLine("O JOGO DUROU " + duracaoHoras + " HORA(S) E " + duracaoMinutos + " MINUTO(S)");
         }
     }
 }
