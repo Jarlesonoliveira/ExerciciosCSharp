4183b47348e5a9bb1d722635e46be7ca6339a63e
Jarleson oliveira <jarlesonmello@gmail.com>
2022-08-15 15:55:36 -0300

    projeto Exec 1049,1050 resolvidos e criado o projeto Exec 1051

--- Exec_1049/Program.cs
+++ Exec_1049/Program.cs
@@ -10,6 +10,39 @@ namespace Exec_1049
     {
         static void Main(string[] args) {
 
+            string VI = Console.ReadLine();
+            string AVIA = Console.ReadLine();
+            string COHH = Console.ReadLine();
+
+            if(VI == "vertebrado") {
+                if(AVIA == "ave") {                    
+                    if(COHH == "carnivoro") {
+                        Console.WriteLine("aguia");
+                    }else if(COHH == "onivoro") {
+                        Console.WriteLine("pomba");
+                    }
+                }else if(AVIA == "mamifero") {
+                    if(COHH == "onivoro") {
+                        Console.WriteLine("homem");
+                    }else if(COHH == "herbivoro") {
+                        Console.WriteLine("vaca");
+                    }
+                }
+            }else if (VI == "invertebrado") {
+                if(AVIA == "inseto") {
+                    if(COHH == "hematofago") {
+                        Console.WriteLine("pulga");
+                    }else if(COHH == "herbivoro") {
+                        Console.WriteLine("lagarta");
+                    }
+                }else if(AVIA == "anelideo") {
+                    if(COHH == "hematofago") {
+                        Console.WriteLine("sanguessuga");
+                    }else if(COHH == "onivoro") {
+                        Console.WriteLine("minhoca");
+                    }
+                }
+            }
         }
     }
 }
