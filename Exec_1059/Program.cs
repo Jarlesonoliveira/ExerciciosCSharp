5597747d14245c119373bdbbaa0b3958d4ca59b4
Jarleson oliveira <jarlesonmello@gmail.com>
2022-08-16 13:54:18 -0300

    projeto Exec 1052,1059,1060 resolvidos e criado o projeto Exec 1061

---
+++ Exec_1059/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_1059
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+            int numPar = 1;
+
+            while(numPar <= 100) {
+
+                if (numPar % 2 == 0) {
+
+                    Console.WriteLine(numPar);
+                } 
+                numPar++;
+            }
+        }
+    }
+}
---
+++ Exec_1060/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_1060
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+                        
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            double num3 = double.Parse(Console.ReadLine());
+            double num4 = double.Parse(Console.ReadLine());
+            double num5 = double.Parse(Console.ReadLine());
+            double num6 = double.Parse(Console.ReadLine());
+
+            int valorPositivo = 0;
+
+            if (num1 > 0.00) {
+                valorPositivo += 1;
+            }
+
+            if (num2 > 0.00) {
+                valorPositivo += 1;
+            }
+
+            if (num3 > 0.00) {
+                valorPositivo += 1;
+            }
+
+            if (num4 > 0.00) {
+                valorPositivo += 1;
+            }
+
+            if (num5 > 0.00) {
+                valorPositivo += 1;
+            }
+
+            if (num6 > 0.00) {
+                valorPositivo += 1;
+            }
+
+            Console.WriteLine(valorPositivo + " valores positivos");
+        }
+    }
+}
