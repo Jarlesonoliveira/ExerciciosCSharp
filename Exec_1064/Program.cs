9c8b920c6b1155de630fd29d14ac0267daab6de0
Jarleson oliveira <jarlesonmello@gmail.com>
2022-08-17 10:59:15 -0300

    projeto Exec 1064 resolvidos e criado o projeto Exec 1065

--- Exec_1064/Program.cs
+++ Exec_1064/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,50 @@ namespace Exec_1064
     {
         static void Main(string[] args) {
 
+            double num1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double num2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double num3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double num4 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double num5 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double num6 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
+            int contagem = 0;
+            double somaValor = 0.00;
+
+            if(num1 >= 0.00) {
+                contagem += 1;
+                somaValor += num1;
+            }
+
+            if (num2 >= 0.00) {
+                contagem += 1;
+                somaValor += num2;
+            }
+
+            if (num3 >= 0.00) {
+                contagem += 1;
+                somaValor += num3;
+            }
+
+            if (num4 >= 0.00) {
+                contagem += 1;
+                somaValor += num4;
+            }
+
+            if (num5 >= 0.00) {
+                contagem += 1;
+                somaValor += num5;
+            }
+
+            if (num6 >= 0.00) {
+                contagem += 1;
+                somaValor += num6;
+            }
+
+            double media = somaValor / contagem;
+
+            Console.WriteLine(contagem + " valores positivos", CultureInfo.InvariantCulture);
+            Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));
         }
     }
 }
