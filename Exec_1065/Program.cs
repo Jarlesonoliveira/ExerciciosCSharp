691041f357319f198d0e6f325bc1b90974e0356f
Jarleson oliveira <jarlesonmello@gmail.com>
2022-08-17 16:46:19 -0300

    projeto Exec 1065,1066 resolvidos e criado o projeto Exec 1067

--- Exec_1065/Program.cs
+++ Exec_1065/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,37 @@ namespace Exec_1065
     {
         static void Main(string[] args) {
 
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+            int num5 = int.Parse(Console.ReadLine());
 
+
+            int contagem = 0;
+
+            if (num1 % 2 == 0.00) {
+                contagem += 1;                
+            }
+
+            if (num2 % 2 == 0.00) {
+                contagem += 1;                
+            }
+
+            if (num3 % 2 == 0.00) {
+                contagem += 1;                
+            }
+
+            if (num4 % 2 == 0.00) {
+                contagem += 1;                
+            }
+
+            if (num5 % 2 == 0.00) {
+                contagem += 1;                
+            }
+
+            Console.WriteLine(contagem + " valores pares");
+            
         }
     }
 }
