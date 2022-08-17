b9923a260f62258add4b86e34d613068c3007d9e
Jarleson oliveira <jarlesonmello@gmail.com>
2022-08-17 18:05:56 -0300

    projeto Exec 1067,1070 resolvidos e criado o projeto Exec 10671

---
+++ Exec_1070/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_1070
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+            int x = int.Parse(Console.ReadLine());
+            int contagem = x;
+
+            if( x % 2 == 0) {
+                contagem++;
+                Console.WriteLine(contagem);
+                contagem += 2;
+                Console.WriteLine(contagem);
+                contagem += 2;
+                Console.WriteLine(contagem);
+                contagem += 2;
+                Console.WriteLine(contagem);
+                contagem += 2;
+                Console.WriteLine(contagem);
+                contagem += 2;
+                Console.WriteLine(contagem);
+
+            } else {
+                Console.WriteLine(x);
+                contagem += 2;
+                Console.WriteLine(contagem);
+                contagem += 2;
+                Console.WriteLine(contagem);
+                contagem += 2;
+                Console.WriteLine(contagem);
+                contagem += 2;
+                Console.WriteLine(contagem);
+                contagem += 2;
+                Console.WriteLine(contagem);
+            }
+        }
+    }
+}
