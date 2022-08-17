4e5eca5f433793e82f50257533db82e5b7ef39db
Jarleson oliveira <jarlesonmello@gmail.com>
2022-08-17 09:50:58 -0300

    projeto Exec 1061 resolvidos e criado o projeto Exec 1064

--- Exec_1061/Program.cs
+++ Exec_1061/Program.cs
@@ -10,7 +10,41 @@ namespace Exec_1061
     {
         static void Main(string[] args) {
 
+            int W, X, Y, Z, resto, diaInicial, diaFinal, inicial, final, duracao;
 
+            string[] dia = (Console.ReadLine()).Split(' ');
+            diaInicial = int.Parse(dia[1]);
+
+            string[] horas = (Console.ReadLine()).Split(':');
+            int horaInicial = int.Parse(horas[0]);
+            int minutoInicial = int.Parse(horas[1]);
+            int segundoInicial = int.Parse(horas[2]);
+
+            inicial = (int) (diaInicial - 1) * 24 * 60 * 60 + horaInicial *60 * 60 + minutoInicial * 60 + segundoInicial;
+
+            dia = (Console.ReadLine()).Split(' ');
+            diaFinal = int.Parse(dia[1]);
+
+            horas = (Console.ReadLine()).Split(':');
+            int horaFinal = int.Parse(horas[0]);
+            int minutoFinal = int.Parse(horas[1]);
+            int segundoFinal = int.Parse(horas[2]);
+
+            final = (int)(diaFinal - 1) * 24 * 60 * 60 + horaFinal * 60 * 60 + minutoFinal * 60 + segundoFinal;
+
+            duracao = final - inicial;
+
+            W = duracao / (24 * 60 * 60);
+            resto = duracao % (24 * 60 * 60);
+            X = resto / (60 * 60);
+            resto = resto % (60 * 60);
+            Y = resto / 60;
+            Z = resto % 60;
+
+            Console.WriteLine(W + " dia(s)");
+            Console.WriteLine(X + " hora(s)");
+            Console.WriteLine(Y + " minuto(s)");
+            Console.WriteLine(Z + " segundo(s)");
         }
     }
 }
