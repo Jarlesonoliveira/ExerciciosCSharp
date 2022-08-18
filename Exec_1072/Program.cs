995a2301e714b0a80c40fe5b3aec2a939499b3c9
Jarleson oliveira <jarlesonmello@gmail.com>
2022-08-18 12:21:41 -0300

    projeto Exec 1071 resolvidos e criado o projeto Exec 10672

---
+++ Exec_1072/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_1072
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+            int N = int.Parse(Console.ReadLine());
+            int X = 0;
+            int contIn = 0, contOut = 0;
+
+            for (int i = 0; i < N; i++) {
+
+                X = int.Parse(Console.ReadLine());
+
+                if (X >= 10 && X <= 20) {
+
+                    contIn++;
+
+                } else {
+
+                    contOut++;
+                }
+            }
+
+            Console.WriteLine(contIn + " em");
+            Console.WriteLine(contOut + " fora");
+        }
+    }
+}
