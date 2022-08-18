e40918a84828dd229e776a2d50d9b4ada409a396
Jarleson oliveira <jarlesonmello@gmail.com>
2022-08-18 17:46:33 -0300

    projeto Exec 1073,1074 resolvido e criado o projeto Exec 1075

---
+++ Exec_1074/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_1074
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+            int N = int.Parse(Console.ReadLine());
+            int X;
+
+            for(int i = 0; i < N; i++) {
+                X = int.Parse(Console.ReadLine());
+                if(X == 0) {
+                    Console.WriteLine("NULL");
+                }else if(X < 0) {
+                    if(X % 2 == 0) {
+                        Console.WriteLine("EVEN NEGATIVE");
+                    } else {
+                        Console.WriteLine("ODD NEGATIVE");
+                    }
+                } else {
+                    if(X % 2 == 0) {
+                        Console.WriteLine("EVEN POSITIVE");
+                    } else {
+                        Console.WriteLine("ODD POSITIVE");
+                    }
+                }
+            }
+
+        }
+    }
+}
