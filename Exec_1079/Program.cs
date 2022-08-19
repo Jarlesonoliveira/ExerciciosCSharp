b850ccdbdd7b0e27a7cdee5f3d6b1e9df6e7c1f9
Jarleson oliveira <jarlesonmello@gmail.com>
2022-08-19 15:41:40 -0300

    projeto Exec 1079 resolvido

---
+++ Exec_1079/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace Exec_1079
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+            int N = int.Parse(Console.ReadLine());
+            float v1, v2, v3;
+
+            for(int i = 0; i < N; i++) {
+
+                string[] vet = Console.ReadLine().Split(' ');
+                v1 = float.Parse(vet[0], CultureInfo.InvariantCulture);
+                v2 = float.Parse(vet[1], CultureInfo.InvariantCulture);
+                v3 = float.Parse(vet[2], CultureInfo.InvariantCulture);
+
+                float m1 = (v1 * 2) / 10;
+                float m2 = (v2 * 3) / 10;
+                float m3 = (v3 * 5) / 10;
+
+                float mediaFinal = m1 + m2 + m3;
+                Console.WriteLine(mediaFinal.ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
