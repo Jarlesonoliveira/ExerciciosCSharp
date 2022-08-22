eaf140870d1eb88f893c85543d4f24cc905624a5
Jarleson oliveira <jarlesonmello@gmail.com>
2022-08-22 17:23:35 -0300

    projeto Exec 1080,1094 resolvido e criado o projeto 1095

---
+++ Exec_1080/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_1080
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+            int numero, maior, posicao;
+
+            maior = 0;
+            posicao = 0;
+
+            for(int i = 1; i <= 100; i++) {
+
+                numero = int.Parse(Console.ReadLine());
+
+                if(numero > maior) {
+
+                    maior = numero;
+                    posicao = i;
+                }
+
+            }
+
+            Console.WriteLine(maior);
+            Console.WriteLine(posicao);
+
+        }
+    }
+}
---
+++ Exec_1094/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_1094
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+            int N = int.Parse(Console.ReadLine());
+
+            int rato = 0;
+            int sapo = 0;
+            int coelho = 0;
+
+            int total = 0;
+
+            for (int i = 0; i < N; i++) {
+                string[] vet = Console.ReadLine().Split(' ');
+
+                int Quantia = int.Parse(vet[0]);
+                char Tipo = char.Parse(vet[1]);
+
+                total += Quantia;
+
+                if (Tipo == 'R') {
+                    rato += Quantia;
+                } else if (Tipo == 'S') {
+                    sapo += Quantia;
+                } else {
+                    coelho += Quantia;
+                }
+            }
+
+            double porcentagemRato = (rato * 100.00) / total;
+            double porcentagemSapo = (sapo * 100.00) / total;
+            double porcentagemCoelho = (coelho * 100.00) / total;
+
+            Console.WriteLine("Total: " + total + " cobaias");
+            Console.WriteLine("Total de coelhos: " + coelho);
+            Console.WriteLine("Total de ratos: " + rato);
+            Console.WriteLine("Total de sapos: " + sapo);
+            Console.WriteLine("Percentual de coelhos: " + porcentagemCoelho.ToString("F2", CultureInfo.InvariantCulture) + " %");
+            Console.WriteLine("Percentual de ratos: " + porcentagemRato.ToString("F2", CultureInfo.InvariantCulture) + " %");
+            Console.WriteLine("Percentual de sapos: " + porcentagemSapo.ToString("F2", CultureInfo.InvariantCulture) + " %");
+
+        }
+    }
+}
