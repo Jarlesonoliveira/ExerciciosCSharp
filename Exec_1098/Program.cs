b821e9c6f7ffcff5112c9f16233664c3e9e46d19
Jarleson oliveira <jarlesonmello@gmail.com>
2022-08-24 11:47:42 -0300

    Exercicio 1098 resolvido

--- Exec_1098/Program.cs
+++ Exec_1098/Program.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
-namespace Exec_1098
+namespace URI_1098
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args) {
 
+            double I = 0.0;
 
+            while (I <= 2.0) {
+                double J = I + 1.0;
+                if (I == 0.0 || I == 1.0) {
+                    for (int e = 0; e < 3; e++) {
+                        Console.WriteLine("I=" + I.ToString(CultureInfo.InvariantCulture) + " J=" + J.ToString(CultureInfo.InvariantCulture));
+                        J++;
+                    }
+                } else {
+                    for (int e = 0; e < 3; e++) {
+                        if (I > 1.9) { 
+                            Console.WriteLine("I=2 J=" + J.ToString(CultureInfo.InvariantCulture));
+                            J++;
+                        } else {
+                            Console.WriteLine("I=" + I.ToString("F1", CultureInfo.InvariantCulture) + " J=" + J.ToString("F1", CultureInfo.InvariantCulture));
+                            J++;
+                        }
+                    }
+                }
+                I += 0.2;
+            }
         }
     }
 }
